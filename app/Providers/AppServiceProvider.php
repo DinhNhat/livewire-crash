483de83abd87c70476b59e87e94ac63dc1ae8eec
DinhNhat <nguyendinhnhat206@gmail.com>
2022-12-10 20:40:29 -0500

    - Update app service path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->bind('path.public', function() {
-            // return your own correct path.
-            return realpath(base_path('../'));
-       });
+    //     $this->app->bind('path.public', function() {
+    //         // return your own correct path.
+    //         return realpath(base_path('../'));
+    //    });
     }
 }
